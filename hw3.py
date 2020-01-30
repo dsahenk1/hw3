c5b4be94521f7c2974b11dc16e6a11542959fa06
dsahenk1 <60157150+dsahenk1@users.noreply.github.com>
2020-01-29 23:33:16 -0500

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,34 @@
+import time
+
+#Exercise1
+#1.1
+a=3
+a+2 
+print("a = 5")
+#1.2
+a= a+1.0
+a
+print("a is 4")
+#1.3 
+a=3
+b
+print("b is null")
+#1.4
+a=3
+a==5.0
+print("a is 5")
+#1.5
+b=10
+c=b>9
+print("c is true")
+#1.6
+5/2==5/2.0
+print("false")
+ #exercise2
+print(3.0 - 1.0 != 5.0 - 3.0)
+print((3 > 4) or (2 < 3 and 9 > 10))
+print(4 > 5 or 3 < 4 and 9 > 8)
+print(not(4 > 3 and 100 > 6))
+
+#script current time 
+
